bfa20410d93e724739cfee030988727c2d28a8d7
Andrew A. Stenipanin <and5251@yandex.ru>
2020-12-22 19:17:16 +0300

    login and cookies

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,16 @@ import express from 'express'
 import morgan from 'morgan'
 import authRoutes from './routes/auth'
 import trim from './middleware/trim'
+import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 
+dotenv.config()
 const app = express()
 
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(trim)
+app.use(cookieParser())
 
 app.get('/', (_, res) => res.send('Hello World'))
 app.use('/api/auth', authRoutes)
